31e532dae837ec663bae459da8d7fdc6991a7085
Donya <thisis2nya@gmail.com>
2022-03-28 04:07:37 +0430

    Comments added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Variables
 const words = ["excited", "beautiful", "thanks", "donya", "honey", "break", "understand", "kill"];
 let picture = document.getElementById("image").querySelector("img");
 let randomWord;
@@ -6,17 +7,17 @@ let wrong = 0;
 let result = "";
 userSelected = []
 
+// create a random word
 function startHangman() {
     randomWord = words[Math.floor(Math.random() * words.length)]
-    // console.log(randomWord)
     letters = randomWord.split("")
     letters = letters.join("")
-    // console.log(letters)
     const letterSection = document.getElementById("letters");
     letterSection.addEventListener("click", buttonHandler);
     window.addEventListener("keydown", keyHandler);
 }
 
+// Put underscore for each letter
 function setUnderScores() {
     let splitedWord = randomWord.split("");
     let mappedWord = splitedWord.map(letter => (userSelected.indexOf(letter) >= 0 ? letter : "_"))
@@ -25,19 +26,21 @@ function setUnderScores() {
     clue.innerText = `${result}`;
 }
 
+// Handle buttons' events
 function buttonHandler(event) {
     letterHandler(event.target.id)
 }
 
+// Handle keys' events
 function keyHandler(event) {
     letterHandler(event.key);
 }
 
+// Check for new letters
 function letterHandler(letter) {
     letter = letter.toLowerCase()
     const usedLetter = document.getElementById(`${letter.toUpperCase()}`);
     usedLetter.className = "used";
-    // console.log(usedLetter)
     userSelected.push(letter);
     if (letters.indexOf(letter) === -1) {
         ifFalse();
@@ -48,10 +51,18 @@ function letterHandler(letter) {
     }
 }
 
+// Check if letter is true
 function ifTrue() {
     setUnderScores();   
 }
 
+// Check if letter is false
+function ifFalse() {
+    wrong++;
+    picture.src = `assets/hangman${wrong}.png`
+}
+
+// Check that the word matches the random word
 function checkIfWon() {
     if (result === randomWord) {
         picture.src = "assets/winner.png";
@@ -59,16 +70,13 @@ function checkIfWon() {
     }
 }
 
-function ifFalse() {
-    wrong++;
-    picture.src = `assets/hangman${wrong}.png`
-}
-
+// Check that the word doesn't match the random word
 function checkIfLost() {
     if (wrong === 6) {
         document.getElementById("clue").querySelector("p").innerText = `Game Over. The word was: ${randomWord}.`
     }
 }
 
+// Start the game
 startHangman();
 setUnderScores();
